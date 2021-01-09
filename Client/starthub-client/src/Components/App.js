5936e09c3138799c2a2c4212a61767cb4dc72377
oEdgarAlves <edgaralves@ua.pt>
2021-01-09 03:38:22 +0000

    fetching data with axious from API

--- Client/starthub-client/src/Components/App.js
+++ Client/starthub-client/src/Components/App.js
@@ -2,12 +2,14 @@ import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import CreateArea from "./CreateArea";
+import PostArea from "./PostArea";
 
 function App(){
     return (
         <div>
             <Header />
             <CreateArea />
+            <PostArea />
             <Footer />
         </div>
     )
--- Client/starthub-client/src/Components/PostArea.js
+++ Client/starthub-client/src/Components/PostArea.js
@@ -1,6 +1,30 @@
-import React from "react";
+import React, { useRef, useCallback, useState } from "react";
+import axious from "axios"
 
 function PostArea(){
+    const [posts, setPosts] = useState([]);
+    const [isLoaded, setisLoaded] = useState(false);
+    const [queryState,setQueryState] = useState({page:1,numberOfPosts:10});
 
-    
+    let URL = `http://localhost:3000/posts/` + queryState.page + '-' + queryState.numberOfPosts;
+
+
+    function handleFetch() {
+        console.log("Entered here")
+        console.log(URL);
+		axious.get(URL).then(function (response) {
+            console.log(response);
+          }).catch(function (error) {
+            // handle error
+            console.log(error);
+          })
+	};
+
+    return (
+        <div className="post" onClick={handleFetch}>
+            <button onClick={handleFetch} />
+        </div>
+    )
 }
+
+export default PostArea;
